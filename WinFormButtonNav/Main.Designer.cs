29a2a72fef366c18c5bce73cac9342a33d1b735d
Lonnie Coates, Jr <lonnie.coates@lemanstech.com>
2020-02-15 17:09:08 -0600

    Initial commit

---
+++ WinFormButtonNav/Main.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace WinFormButtonNav
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.chbNavToggle = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(144, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(331, 126);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 38);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(505, 126);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 38);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(210, 232);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(133, 38);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(436, 232);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(133, 38);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnExit.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnExit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnExit.Location = new System.Drawing.Point(601, 378);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(133, 38);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // chbNavToggle
+            // 
+            this.chbNavToggle.AutoSize = true;
+            this.chbNavToggle.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.chbNavToggle.Location = new System.Drawing.Point(664, 65);
+            this.chbNavToggle.Name = "chbNavToggle";
+            this.chbNavToggle.Size = new System.Drawing.Size(112, 17);
+            this.chbNavToggle.TabIndex = 5;
+            this.chbNavToggle.Text = "Natural Navigation";
+            this.chbNavToggle.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.chbNavToggle.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chbNavToggle);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.KeyPreview = true;
+            this.Name = "Main";
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.CheckBox chbNavToggle;
+    }
+}
+
---
+++ WinFormButtonNav/Main.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WinFormButtonNav
+{
+    public partial class Main : Form
+    {
+        Main frmMain;
+        public Main()
+        {
+            frmMain = this;
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            LoadButtonList();
+        }
+
+        List<Control> ButtonList;
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void LoadButtonList()
+        {
+            ButtonList = new List<Control>();
+            // Button List must be sorted first by X axis then by Y axis for natural key nav to work
+            ButtonList = Controls.Cast<Control>().Where(c=> c is Button || c is CheckBox).OrderBy(i=> i.Location.X).OrderBy(i=> i.Location.Y).ToList();
+            //ButtonList = Controls.OfType<Button>().Cast<Control>().OrderBy(i => i.Location.Y).ToList();
+            // needs to record Location X & Y
+
+            //ButtonList.ForEach(c => c.KeyDown += new KeyEventHandler(Button_KeyDown));
+
+        }
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            // See link below for source of this function
+            // https://stackoverflow.com/a/34168026
+            /*
+                37	left arrow
+                38	up arrow
+                39	right arrow
+                40	down arrow
+             */
+
+            var allControls = GetAllFormControls(frmMain);
+
+            var current = GetFocusedControl(allControls, frmMain);
+            var c = current.GetType();
+            if (chbNavToggle.Checked && (current is Button || current is Form || current is CheckBox))
+            {
+                switch (keyData)
+                {
+                    case Keys.Left:
+                        NextControl(ButtonList, "LEFT");
+                        return true;
+                    case Keys.Right:
+                        NextControl(ButtonList, "RIGHT");
+                        return true;
+                    case Keys.Up:
+                        NextControl(ButtonList, "UP");
+                        return true;
+                    case Keys.Down:
+                        NextControl(ButtonList, "DOWN");
+                        return true;
+                    default:
+                        break;
+                }
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+        public void Button_KeyDown(object sender = null, KeyEventArgs e = null)
+        {
+            
+        }
+
+        public List<Control> GetAllFormControls(Form myForm)
+        {
+            var myControls = myForm.Controls.Cast<Control>().ToList();
+            return myControls;
+        }
+
+        public Control GetFocusedControl (List<Control> myControls, Form myForm)
+        {
+            var result = myControls.Where(c => c.Focused == true).FirstOrDefault();
+            if (result == null)
+            {
+                if (myForm.Focused)
+                {
+                    result = myForm;
+                }
+            }
+            return result;
+        }
+
+        public void NextControl (List<Control> myControls, string direction)
+        {
+            var elementNo = myControls.FindIndex(c => c.Focused == true);
+            Control newControl = null;
+            List<Control> newControls = null;
+            var currentControl = myControls[elementNo] == null ? GetFocusedControl(myControls, frmMain): myControls[elementNo];
+            switch (direction)
+            {
+                // Case UP is bugging
+                case "LEFT":
+                    newControls = myControls.Where(p => currentControl.Location.Y == p.Location.Y & currentControl.Location.X > p.Location.X).OrderByDescending(i => i.Location.X).ToList();
+                    if (newControls.Count == 0)
+                    {
+                        newControls = myControls.Where(p => currentControl.Location.X > p.Location.X).OrderBy(i => Math.Abs(currentControl.Location.Y - i.Location.Y)).ToList();
+                    }
+                    break;
+                case "RIGHT":
+                    newControls = myControls.Where(p => currentControl.Location.Y == p.Location.Y & currentControl.Location.X < p.Location.X).OrderBy(i => i.Location.X).ToList();
+                    if (newControls.Count == 0)
+                    {
+                        newControls = myControls.Where(p => currentControl.Location.X < p.Location.X).OrderBy(i => Math.Abs(currentControl.Location.Y - i.Location.Y)).ToList();
+                    }
+                    break;
+                case "UP":
+                    newControls = myControls.Where(p => currentControl.Location.X == p.Location.X & currentControl.Location.Y > p.Location.Y).OrderByDescending(i => i.Location.Y).ToList();
+                    if (newControls.Count == 0)
+                    {
+                        newControls = myControls.Where(p => currentControl.Location.Y > p.Location.Y).OrderBy(i => Math.Abs(currentControl.Location.X - i.Location.X)).ToList();
+                    }
+                    
+                    break;
+                case "DOWN":
+                    newControls = myControls.Where(p => currentControl.Location.X == p.Location.X & currentControl.Location.Y < p.Location.Y).OrderBy(i => i.Location.Y).ToList();
+                    if (newControls.Count == 0)
+                    {
+                        //newControls = myControls.Where(p => currentControl.Location.Y < p.Location.Y).OrderBy(i => i.Location.Y).ToList();
+                        newControls = myControls.Where(p => currentControl.Location.Y < p.Location.Y).OrderBy(i => Math.Abs(currentControl.Location.X - i.Location.X)).ToList();
+                    }
+                    break;
+                default:
+                    break;
+            }
+            newControl = newControls.FirstOrDefault();
+            if (newControl != null)
+            {
+                newControl.Focus();
+            }
+        }
+        
+    }
+}
